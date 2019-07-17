67aea903aae6a21f6d93c58a102061bc67150aab
DmJSCourse <dimonua@gmail.com>
2019-07-17 17:09:27 +0300

    dop zadanie

--- l7.js
+++ l7.js
@@ -11,8 +11,8 @@ if (backupCats && backupCats.length) {
 
 function buildCats() {
     var catsStr = '';
-    for(var cat of cats) {
-        catsStr += '<div class="itembox" style="float: left;">' + cat.name + " " + "(" + cat.age + ')<button style="float:right;" id="delete">Удалить</button><br><img src="' + cat.link + '"></div>';
+    for (var cat of cats) {
+        catsStr += '<div class="itembox" style="float: left;"><span class="namebox">' + cat.name + "</span>" + " " + "(" + cat.age + ')<button style="float:right;" id="delete">Удалить</button><br><img src="' + cat.link + '"></div>';
     }
     $('#cats').html(catsStr);
     $('input').val('');
@@ -33,19 +33,23 @@ function addCat() {
     localStorage.setItem('cats', catsStr);
 }
 
-// $('.itembox button').on('click', function(e){
-//     console.log($(this).parent().index());
-//     cats.splice($(this).parent().index(), 1);
-//     $(this).parent().remove();
-//     console.log(cats);
-//     var catsStr = JSON.stringify(cats);
-//     console.log(catsStr);
-//     localStorage.setItem('cats', catsStr);
-// });
-
 $(document).on('click', '.itembox button', function (e) {
     cats.splice($(this).parent().index(), 1);
     $(this).parent().remove();
     var catsStr = JSON.stringify(cats);
     localStorage.setItem('cats', catsStr);
+});
+
+$(document).on('dblclick', '.itembox span', function (e) {
+    $(this).html('<input type="text" style ="width:140px;" id="editNameBox"></input>');
+});
+
+
+$(document).on('keydown', '#editNameBox', function (e) {
+    if (e.keyCode == 13) {
+        cats[$(this).parent().parent().index()].name = $('#editNameBox').val();
+        buildCats();
+        var catsStr = JSON.stringify(cats);
+        localStorage.setItem('cats', catsStr);
+    }
 });
